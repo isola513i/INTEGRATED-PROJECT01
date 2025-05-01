1057a595d263ab2bc1039e9f272c7dcf2a662d08
Macbook-Isolicez <okuma530@gmail.com>
2025-05-01 20:38:49 +0700

    031 - update component import paths for better organization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
+import Navbar from './components/UI/Navbar.vue';
+import Footer from './components/UI/Footer.vue';
 </script>
 <template>
   <div class="bg-[#ffffff]">
--- frontend/src/views/ProductGalleryView.vue
+++ frontend/src/views/ProductGalleryView.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { computed } from 'vue';
-import SaleItemCard from '@/components/SaleItemCard.vue';
+import SaleItemCard from '@/components/ProductGallery/SaleItemCard.vue';
 import saleItems from '@/mocks/sale-items.json';
 import brands from '@/mocks/brands.json';
-import ProductFilter from '@/components/ProductFilter.vue';
-import ProductCarousel from '@/components/ProductCarousel.vue';
-import PromoBar from '@/components/PromoBar.vue';
+import ProductFilter from '@/components/ProductGallery/ProductFilter.vue';
+import ProductCarousel from '@/components/ProductGallery/ProductCarousel.vue';
+import PromoBar from '@/components/ProductGallery/PromoBar.vue';
 
 const sortedItems = computed(() => {
   return saleItems
