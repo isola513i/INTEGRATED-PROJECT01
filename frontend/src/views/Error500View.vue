e86d52dbddc43adb5bdb85875e033b38d18d89b1
Macbook-Isolicez <okuma530@gmail.com>
2025-05-02 23:37:37 +0700

    031- Update fetch API by using axios  frontend dependencies and configuration files

---
+++ frontend/src/views/Error500View.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center text-center bg-white"
+  >
+    <h1 class="text-5xl font-bold text-red-700 mb-4">500 - Server Error</h1>
+    <p class="text-gray-700 mb-6">Oops! Something went wrong on our side.</p>
+    <router-link to="/" class="btn btn-error">Back to home</router-link>
+  </div>
+</template>
