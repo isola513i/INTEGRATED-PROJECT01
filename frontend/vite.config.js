7a4f7a31411378bfdf6426b4cc321257cecc9606
Macbook-Isolicez <okuma530@gmail.com>
2025-05-03 20:59:54 +0700

    031 - Update API endpoint in saleItemService and ProductGalleryView

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,16 +12,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://ip24ssi4.sit.kmutt.ac.th:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/itb-mshop/v1'),
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest',
-        },
-      },
-    },
-  },
 });
