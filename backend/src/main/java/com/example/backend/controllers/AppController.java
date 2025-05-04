c0af633b1cec3172e05b986543cbe9ee6e764e3f
dear <chakhrit.phu@gmail.com>
2025-05-04 09:38:59 +0700

    023-[update] cross origins , properties file

--- backend/src/main/java/com/example/backend/controllers/AppController.java
+++ backend/src/main/java/com/example/backend/controllers/AppController.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
+@CrossOrigin(origins = "${app.cors.allowedOrigins}")
 @RestController
-@RequestMapping("/itb-mshop")
+@RequestMapping("/itb-mshop/vi")
 public class AppController {
     @Autowired
     private SaleItemService saleItemService;
@@ -24,11 +25,11 @@ public class AppController {
     @Autowired
     private ListMapper listMapper;
 
-    @GetMapping("v1/sale-items")
+    @GetMapping("/sale-items")
     public ResponseEntity<List<SaleItemDto.GetAllSaleItemsDto>> getAllItems(){
         return ResponseEntity.ok(listMapper.mapList(saleItemService.allSaleItems(),SaleItemDto.GetAllSaleItemsDto.class,modelMapper));
     }
-    @GetMapping("/v1/sale-items/{saleItemId}")
+    @GetMapping("/sale-items/{saleItemId}")
     public ResponseEntity<SaleItemDto.GetSaleItemDto> getSaleItemById(@PathVariable Integer saleItemId){
         return ResponseEntity.ok(modelMapper.map(saleItemService.findSaleItemById(saleItemId), SaleItemDto.GetSaleItemDto.class));
     }
