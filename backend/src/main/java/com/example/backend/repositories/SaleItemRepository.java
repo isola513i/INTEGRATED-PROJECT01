3b3b35d104bb079f6ec1b9bb93e8580eae77a681
dear <chakhrit.phu@gmail.com>
2025-05-05 08:57:49 +0700

    023-[update] repository

--- backend/src/main/java/com/example/backend/repositories/SaleItemRepository.java
+++ backend/src/main/java/com/example/backend/repositories/SaleItemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SaleItemRepository extends JpaRepository<SaleItem,Integer> {
-    public List<SaleItem> findAllByOrderByCreatedOnAsc();
+    public List<SaleItem> findAllByOrderByCreatedOnAscIdAsc();
 }
