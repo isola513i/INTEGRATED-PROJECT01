aa7f4bcaa9b5bfa2107fdfffb552a84e6d2f42f6
Nattapat Lamnui <okuma530@gmail.com>
2025-05-21 03:25:37 +0700

    031 - Add SaleItemControllerV2 and implement sorting for sale items

---
+++ backend/src/main/java/com/example/backend/controllers/SaleItemControllerV2.java
@@ -0,0 +1,40 @@
+package com.example.backend.controllers;
+
+import com.example.backend.dtos.SaleItemDto;
+import com.example.backend.entities.SaleItem;
+import com.example.backend.services.BrandService;
+import com.example.backend.services.SaleItemService;
+import com.example.backend.utils.ListMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("itb-mshop/v2")
+@RestController
+public class SaleItemControllerV2 {
+    @Autowired
+    private SaleItemService saleItemService;
+    @Autowired
+    private BrandService brandService;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private ListMapper listMapper;
+
+    @GetMapping("/sale-items")
+    public ResponseEntity<List<SaleItemDto.GetAllSaleItemsDto>> getSortedItems(
+            @RequestParam(required = false) String sortField,
+            @RequestParam(defaultValue = "asc") String sortDirection
+    ) {
+        List<SaleItem> saleItems = saleItemService.getSaleItemsSorted(sortField, sortDirection);
+        return ResponseEntity.ok(
+                listMapper.mapList(saleItems, SaleItemDto.GetAllSaleItemsDto.class, modelMapper)
+        );
+    }
+}
