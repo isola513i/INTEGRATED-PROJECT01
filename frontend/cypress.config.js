2822de14752b9ebdadfa700c77e31681e45712fb
dear <chakhrit.phu@gmail.com>
2025-05-29 23:39:21 +0700

    023-[update] refactor code backend and frontend

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://intproj24.sit.kmutt.ac.th/ssi4/',
-    baseAPI: 'http://intproj24.sit.kmutt.ac.th/ssi4/itb-mshop',
-    // baseUrl: 'http://localhost:5173/ssi4/',
-    // baseAPI: 'http://localhost:8080/itb-mshop',
+    //baseUrl: 'http://intproj24.sit.kmutt.ac.th/ssi4/',
+    //baseAPI: 'http://intproj24.sit.kmutt.ac.th/ssi4/itb-mshop',
+     baseUrl: 'http://localhost:5173/ssi4/',
+     baseAPI: 'http://localhost:8080/itb-mshop',
     experimentalRunAllSpecs:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
