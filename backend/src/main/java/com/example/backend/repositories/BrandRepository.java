7be165724c5244a70229ea2ffed3d03008ed7212
dear <chakhrit.phu@gmail.com>
2025-05-29 20:40:39 +0700

    023-[update] refactor code back and front

--- backend/src/main/java/com/example/backend/repositories/BrandRepository.java
+++ backend/src/main/java/com/example/backend/repositories/BrandRepository.java
@@ -3,8 +3,7 @@ package com.example.backend.repositories;
 import com.example.backend.entities.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
+
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
     boolean existsByNameAndIdNot(String name,Integer id);
