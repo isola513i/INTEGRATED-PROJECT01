db88148592ea60ebdb64b851534cdc63ae6552d1
dear <chakhrit.phu@gmail.com>
2025-05-29 23:42:03 +0700

    023-[update] refactor code front and back

--- backend/src/main/java/com/example/backend/controllers/BrandController.java
+++ backend/src/main/java/com/example/backend/controllers/BrandController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = "*")
+
 @RestController
 @RequestMapping("/itb-mshop/v1")
 public class BrandController {
